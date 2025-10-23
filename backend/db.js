3649c0a977ae2c14b0f44f6de07464bf1056ffc5
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-10-23 16:48:52 -0400

    changed backend files v2

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 let db;
 
